91a81cf302c7073122e2541fee4344cf49fca1f4
VandreiMP <vandrei.masiero.padilha@gmail.com>
2023-05-29 20:18:24 -0300

    Initial commit

---
+++ test/transaction-app.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const TransactionApp = require('../lib/transaction-app-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/transaction-app-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new TransactionApp.TransactionAppStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
